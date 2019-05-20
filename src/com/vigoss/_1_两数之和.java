f420f62326cd2173ca87b71f8e0cb5c174a6afe8
weishi.qi <weishi.qi@accenture.com>
2019-05-20 14:27:26 +0800

    init 1.暴力法 2.一遍哈希表

--- src/com/vigoss/_1_两数之和.java
+++ src/com/vigoss/_1_两数之和.java
@@ -9,24 +9,35 @@ import java.util.*;
 public class _1_两数之和 {
   public static int[] twoSum(int[] nums, int target) {
     int[] result = new int[2];
-    int[] ints = nums.clone();
+    List<Integer> list = new ArrayList<>();
+    Map<Integer, Integer> targetMap = new HashMap<>(nums.length);
+    // 1.需要按数组下标顺序依次返回下标
+    // 2.如果数组中存在相同的数字相加等于target
+
+    //    for (int i = 0; i < nums.length; i++) {
+    //      for (int j = 0; j < nums.length; j++) {
+    //        if (nums[i] + nums[j] == target && i != j) {
+    //          result[0] = j;
+    //          result[1] = i;
+    //          break;
+    //        }
+    //      }
+    //    }
 
     for (int i = 0; i < nums.length; i++) {
-      for (int i1 = 0; i1 < ints.length; i1++) {
-        if (nums[i] + ints[i1] == target && i != i1) {
-          result[0] = i;
-          result[1] = i1;
-          break;
-        }
+      int cmp = target - nums[i];
+      if (targetMap.containsKey(cmp)) {
+        return new int[] {targetMap.get(cmp), i};
       }
+      targetMap.put(nums[i], i);
     }
 
-    return result;
+    return null;
   }
 
   public static void main(String[] args) {
-    int[] ints = new int[] {3, 3};
-    int[] ints1 = twoSum(ints, 6);
+    int[] ints = new int[] {2, 7,2, 11, 15};
+    int[] ints1 = twoSum(ints, 4);
     System.out.println(Arrays.toString(ints1));
   }
 }
