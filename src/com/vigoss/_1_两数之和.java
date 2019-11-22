7f18f214e2bb61a66bf5541f16737ce0208a8d3f
weishi.qi <weishi.qi@accenture.com>
2019-11-22 19:17:40 +0800

    347 前k个高频元素

--- src/com/vigoss/_1_两数之和.java
+++ src/com/vigoss/_1_两数之和.java
@@ -7,37 +7,32 @@ import java.util.*;
  * @date 2019/5/10
  */
 public class _1_两数之和 {
-  public static int[] twoSum(int[] nums, int target) {
-    int[] result = new int[2];
-    List<Integer> list = new ArrayList<>();
-    Map<Integer, Integer> targetMap = new HashMap<>(nums.length);
-    // 1.需要按数组下标顺序依次返回下标
-    // 2.如果数组中存在相同的数字相加等于target
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     *
+     * <p>你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> targetMap = new HashMap<>(nums.length);
 
-    //    for (int i = 0; i < nums.length; i++) {
-    //      for (int j = 0; j < nums.length; j++) {
-    //        if (nums[i] + nums[j] == target && i != j) {
-    //          result[0] = j;
-    //          result[1] = i;
-    //          break;
-    //        }
-    //      }
-    //    }
+        for (int i = 0; i < nums.length; i++) {
+            int cmp = target - nums[i];
+            if (targetMap.containsKey(cmp)) {
+                return new int[]{targetMap.get(cmp), i};
+            }
+            targetMap.put(nums[i], i);
+        }
 
-    for (int i = 0; i < nums.length; i++) {
-      int cmp = target - nums[i];
-      if (targetMap.containsKey(cmp)) {
-        return new int[] {targetMap.get(cmp), i};
-      }
-      targetMap.put(nums[i], i);
+        return null;
     }
 
-    return null;
-  }
-
-  public static void main(String[] args) {
-    int[] ints = new int[] {2, 7,2, 11, 15};
-    int[] ints1 = twoSum(ints, 4);
-    System.out.println(Arrays.toString(ints1));
-  }
+    public static void main(String[] args) {
+        int[] ints = new int[]{2, 7, 2, 11, 15};
+        int[] ints1 = twoSum(ints, 4);
+        System.out.println(Arrays.toString(ints1));
+    }
 }
